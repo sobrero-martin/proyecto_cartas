82460995c024385508459409a55ef8f93ffe9418
sobrero-martin <sobrero.martin26@gmail.com>
2025-05-02 19:08:08 -0300

    Placeholder navigation bar and menus

---
+++ scripts/MainUi.cs
@@ -0,0 +1,45 @@
+using Godot;
+using System;
+
+public partial class MainUi : Control
+{ 
+	
+	private string lastMenu;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		lastMenu = "MainMenu"; 
+	}
+	
+	private void SwitchMenu(string hideMenu, string newMenu){
+		GetNode<Control>("UIContainer/CurrentMenu/" + hideMenu).Hide();
+		GetNode<Control>("UIContainer/CurrentMenu/" + newMenu).Show();
+		lastMenu = newMenu;
+	}
+	
+	private void ButtonMainPressed(){
+		GD.Print("This is ButtonMain");
+ 		SwitchMenu(lastMenu, "MainMenu");
+	}
+	
+	private void ButtonShopPressed(){
+		GD.Print("This is ButtonShop");
+		SwitchMenu(lastMenu, "ShopMenu");
+	}
+	
+	private void ButtonInvPressed(){
+		GD.Print("This is ButtonInv");
+		
+	}
+	
+	private void ButtonFriendsPressed(){
+		GD.Print("This is ButtonFriends");
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
