5a940df4ebdc45432a7834ce1d68e48dcf0c0c2c
Lautaro Montoya <134419622+LautaroM2332@users.noreply.github.com>
2025-05-06 20:37:12 -0300

    Inventory

---
+++ scripts/inventoryScript.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class inventoryScript : Control
+{
+	private void Card1()
+	{
+		GD.Print("boton1");
+		
+	}
+	
+	private void Card2()
+	{
+		GD.Print("boton2");
+	}
+	
+	private void Card3()
+	{
+		GD.Print("boton3");
+	}
+	
+	private void Card4()
+	{
+		GD.Print("boton4");
+	}
+	
+	private void Card5()
+	{
+		GD.Print("boton5");
+	}
+	
+	private void Card6()
+	{
+		GD.Print("Boton6");
+	}
+}
